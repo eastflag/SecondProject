b0c2197e3fb1cf869d400b140b50d41900aff360
ace001 <jeteaime012@naver.com>
2016-04-23 16:55:48 +0900

    7주차 - 스케치앱
    펜사이즈 커스텀 다이얼로그 만들기

--- app/src/main/java/com/eastflag/secondproject/fragment/SchetchFragment.java
+++ app/src/main/java/com/eastflag/secondproject/fragment/SchetchFragment.java
@@ -1,7 +1,9 @@
 package com.eastflag.secondproject.fragment;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -10,17 +12,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.eastflag.secondproject.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SchetchFragment extends Fragment {
     @Bind(R.id.container) LinearLayout container;
+    @Bind(R.id.tvPenSize) TextView tvPenSize;
+    private int mProgress = 5; //메인화면의 펜사이즈 값
 
     public SchetchFragment() {
         // Required empty public constructor
@@ -32,6 +38,8 @@ public class SchetchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_schetch, container, false);
         ButterKnife.bind(this, view);
 
+        tvPenSize.setText(String.valueOf(mProgress));
+
         //커스텀뷰를 동적으로 레이아웃에 추가
         //1) 인스턴스 생성
         MyView myView = new MyView(getActivity());
@@ -42,6 +50,33 @@ public class SchetchFragment extends Fragment {
         container.addView(myView, params);
 
         return view;
+    }
+
+    @OnClick(R.id.btnPenSize)
+    void btnPenSize() {
+        //1. 메인의 값(mProgress)을 읽어서 대화창의 seekBar와 텍스트뷰에 표현하기
+        //2. 시크바 리스너 구현하기
+        //3. ok버튼 클릭시 메인에 값 반영하기
+
+        View view = View.inflate(getActivity(), R.layout.popup_pensize, null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("펜사이즈")
+                //.setMessage("메시지내용"), 메시지를 커스터마이징하는 setView가 제공.
+                .setView(view)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
     }
 
     class MyView extends View {
