486cec65eab6526e578eba6a9baf548a4daf52e3
ace001 <jeteaime012@naver.com>
2016-04-30 16:08:24 +0900

    8주차 - 위치기반 검색
    현재위치 가져와서 현재위치로 지도 이동
    롤리팝 이슈 해결

--- app/src/main/java/com/eastflag/secondproject/fragment/PlaceFragment.java
+++ app/src/main/java/com/eastflag/secondproject/fragment/PlaceFragment.java
@@ -1,19 +1,35 @@
 package com.eastflag.secondproject.fragment;
 
 
+import android.Manifest;
+import android.app.FragmentManager;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.eastflag.secondproject.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlaceFragment extends Fragment {
-
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation; //현재위치를 담고 있는 객체
+    private GoogleMap mGoogleMap;
 
     public PlaceFragment() {
         // Required empty public constructor
@@ -23,8 +39,69 @@ public class PlaceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_place, container, false);
+        View view = inflater.inflate(R.layout.fragment_place, container, false);
+
+        //구글 api 엑세스
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addConnectionCallbacks(mConnectionCallbacks)
+                    .addOnConnectionFailedListener(mOnConnectionFailedListener)
+                    .addApi(LocationServices.API)
+                    .build();
+            mGoogleApiClient.connect();
+        }
+
+        return view;
     }
 
+    private void displayMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        //mGoogleMap = mapFragment.getMap();
+        mGoogleMap = getMapFragment().getMap();
+
+        //현재위치로 지도를 이동하고 zoom을 16레벨로 설정
+        mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude()), 16));
+    }
+
+    private MapFragment getMapFragment() {
+        FragmentManager fm = null;
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            fm = getFragmentManager();
+        } else {
+            fm = getChildFragmentManager();
+        }
+        return (MapFragment) fm.findFragmentById(R.id.map);
+    }
+
+    GoogleApiClient.ConnectionCallbacks mConnectionCallbacks = new GoogleApiClient.ConnectionCallbacks() {
+        @Override
+        public void onConnected(Bundle bundle) {
+            Log.d("LDK", "connected");
+            //가장 최근 위치 가져오기
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+            if (mLastLocation != null) {
+                Log.d("LDK", mLastLocation.getLatitude() + "," + mLastLocation.getLongitude());
+                displayMap();
+            }
+        }
+
+        @Override
+        public void onConnectionSuspended(int i) {
+            Log.d("LDK", "connectionSuspended");
+        }
+    };
+
+    GoogleApiClient.OnConnectionFailedListener mOnConnectionFailedListener = new GoogleApiClient.OnConnectionFailedListener() {
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+            Log.d("LDK", "connect fail");
+        }
+    };
 }
